e7a936a596eb75566fbad19fd37e31fdf3a55103
Sam Burville <samburville@Sams-MacBook-Pro.local>
2020-03-04 16:30:18 +0000

    Move asking a question logic to own func
    This reduces the duplication on asking questions

--- nim/nim.go
+++ nim/nim.go
@@ -18,27 +18,9 @@ type GameState struct {
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("How many piles do you want your game to have?")
-	userInputPiles, _ := reader.ReadString('\n')
+	piles := AskPlayerForInt("How many piles do you want your game to have?", reader)
 
-	userInputPiles = strings.Replace(userInputPiles, "\n", "", -1)
-
-	piles, err := strconv.Atoi(userInputPiles)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("How many matches do you want each pile to have?")
-	userInputMatches, _ := reader.ReadString('\n')
-
-	userInputMatches = strings.Replace(userInputMatches, "\n", "", -1)
-
-	matches, err := strconv.Atoi(userInputMatches)
-
-	if err != nil {
-		panic(err)
-	}
+	matches := AskPlayerForInt("How many matches do you want each pile to have?", reader)
 
 	gameState := NewGameState(piles, matches)
 	fmt.Println()
@@ -54,31 +36,14 @@ func main() {
 
 func (gameState *GameState) playMove(reader *bufio.Reader) {
 
-	fmt.Println("Which pile would you like to remove from?")
-	userInputPile, _ := reader.ReadString('\n')
-
-	userInputPile = strings.Replace(userInputPile, "\n", "", -1)
-
-	pile, err := strconv.Atoi(userInputPile)
-
-	if err != nil {
-		panic(err)
-	}
+	pile := AskPlayerForInt("Which pile would you like to remove from?", reader)
 
 	if pile > len(gameState.gameBoard) {
 		panic(errors.New("Pile is out of range of the game board"))
 	}
 
-	fmt.Println("How many matches would you like to remove?")
-	userInputMatches, _ := reader.ReadString('\n')
+	matches := AskPlayerForInt("How many matches would you like to remove?", reader)
 
-	userInputMatches = strings.Replace(userInputMatches, "\n", "", -1)
-
-	matches, err := strconv.Atoi(userInputMatches)
-
-	if err != nil {
-		panic(err)
-	}
 	matchesToRemove := len(gameState.gameBoard[pile-1]) - matches
 
 	if matchesToRemove < 0 {
@@ -126,4 +91,20 @@ func NewGameState(piles, matches int) *GameState {
 	isGameover := piles == 1 && matches == 1
 
 	return &GameState{gameBoard, isGameover}
+}
+
+// AskPlayerForInt ask a player a question and expect an integer response
+func AskPlayerForInt(question string, reader *bufio.Reader) int {
+	fmt.Println(question)
+	userInput, _ := reader.ReadString('\n')
+
+	userInput = strings.Replace(userInput, "\n", "", -1)
+
+	userInputAsInteger, err := strconv.Atoi(userInput)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return userInputAsInteger
 }
