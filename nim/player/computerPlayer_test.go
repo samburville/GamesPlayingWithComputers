88de1030eebddc7d52413425fc2e99bc926d5936
Sam Burville <samburville@protonmail.com>
2020-04-08 20:59:28 +0100

    Add tests for NimSum private method
    This seems to be working correctly. No major edge cases, but seems OK

---
+++ nim/player/computerPlayer_test.go
@@ -0,0 +1,24 @@
+package player
+
+import "testing"
+
+func Test_calculateNimSum(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  int
+	}{
+		{"1 pile board case 1", []int{1}, 1},
+		{"1 pile board case 2", []int{10}, 10},
+		{"2 pile board case 1", []int{1, 1}, 0},
+		{"3 pile board case 1", []int{2, 3, 4}, 5},
+		{"3 pile board case 2", []int{1, 0, 8}, 9},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculateNimSum(tt.input); got != tt.want {
+				t.Errorf("calculateNimSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
